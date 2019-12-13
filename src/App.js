8059359afece21ac8871504b70f9c924eaf9b17d
Nobu <rockmusic2090@gmail.com>
2019-12-12 17:55:13 -0800

    added code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useReducer } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import ProductsPage from './pages/Products';
-import CartPage from './pages/Cart';
-import './App.css';
+import ProductsPage from "./pages/Products";
+import CartPage from "./pages/Cart";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
+import reducer, { initialState } from "./store/reducers";
+import ShopContext from "./context/shopContext";
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <ShopContext.Provider
+      value={{
+        state,
+        dispatch
+      }}
+    >
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ProductsPage} exact />
-          <Route path="/cart" component={CartPage} exact />
+          <Route path="/" render={() => <ProductsPage />} exact />
+          <Route path="/cart" render={() => <CartPage />} exact />
         </Switch>
       </BrowserRouter>
-    );
-  }
+    </ShopContext.Provider>
+  );
 }
 
 export default App;
